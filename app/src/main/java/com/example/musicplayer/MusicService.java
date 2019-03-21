ca4eb3afcf5c7e8b1a3c9165177d73d91414f23a
MinhHoang2003 <hoangd16ptit@gmail.com>
2019-03-21 09:02:45 +0700

    custom notification

--- app/src/main/java/com/example/musicplayer/MusicService.java
+++ app/src/main/java/com/example/musicplayer/MusicService.java
@@ -1,14 +1,21 @@
 package com.example.musicplayer;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.example.musicplayer.model.Song;
 
@@ -16,6 +23,7 @@ import java.util.ArrayList;
 
 public class MusicService extends Service {
     public static final String TAG = MusicService.class.getSimpleName();
+    public static final String CHANEL_ID = "background_music";
     private ArrayList<Song> listSong;
 
     private MediaPlayer player;
@@ -27,7 +35,8 @@ public class MusicService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        readMediaFile();
+      //  readMediaFile();
+
         player = MediaPlayer.create(this,R.raw.onrainyday_beats);
         player.setLooping(true);
 
@@ -36,7 +45,9 @@ public class MusicService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         player.start();
-
+        startForeground(2,createNotification());
+//        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        manager.notify(1,createNotification());
         return super.onStartCommand(intent, flags, startId);
 
     }
@@ -71,11 +82,33 @@ public class MusicService extends Service {
                 }
             }
         }
-        String test = "";
-        for (int i = 0; i < 10; i++) {
-            test = test + " " + listSong.get(i).getName();
-        }
-        Log.d(TAG, "readMediaFile: " + listSong.size());
 
     }
+
+    private Notification createNotification(){
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+
+            NotificationChannel channel = new NotificationChannel(CHANEL_ID,
+                    "Music Control",
+                    NotificationManager.IMPORTANCE_HIGH);
+            manager.createNotificationChannel(channel);
+        }
+
+        RemoteViews customNoti = new RemoteViews(getPackageName(),R.layout.custom_notification);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,0,intent,0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANEL_ID)
+                .setSmallIcon(R.drawable.ic_queue_music_black_24dp)
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                .setCustomContentView(customNoti)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true);
+
+        return builder.build();
+    }
+
 }
