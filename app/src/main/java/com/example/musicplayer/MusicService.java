34b6aa9d1c88c7250a8837eecaba2e40baf76cba
MinhHoang2003 <hoangd16ptit@gmail.com>
2019-04-04 10:06:24 +0700

    using bundservice to update timer

--- app/src/main/java/com/example/musicplayer/MusicService.java
+++ app/src/main/java/com/example/musicplayer/MusicService.java
@@ -6,77 +6,109 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
-import android.provider.MediaStore;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.RemoteViews;
 
+import com.example.musicplayer.model.PlayList;
 import com.example.musicplayer.model.Song;
 
-import java.util.ArrayList;
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener {
 
-public class MusicService extends Service {
-
-    //Action in notification
+    //action get from activity and notification
+    public static final String SUPPLY = "com.example.musicplayer.supply";
     public static final String NEXT = "com.example.musicplayer.next";
     public static final String PAUSE = "com.example.musicplayer.pause";
     public static final String PREVIOUS = "com.example.musicplayer.previous";
-    //ibinder
-    private IBinder iBinder = new MusicBinder();
-    public static final String TAG = MusicService.class.getSimpleName();
-    public static final String CHANEL_ID = "background_music";
-    private ArrayList<Song> listSong;
+    public static final String PREPARED = "com.example.musicplayer.prepared";
+    public static final String PLAY = "com.example.musicplayer.play";
+    public static final String PLAY_ONCLICK = "com.example.musicplayer.playonclickitem";
 
-    private MediaPlayer player;
-    private BroadcastReceiver receiver = receiver = new BroadcastReceiver() {
+    //key
+    public static final String DURATION_SONG = "com.example.musicplayer.durration";
+    public static final String SEEK_TO = "com.example.musicplayer.seekto";
+
+    // to update timer
+    private TimeCurrentBinder mTimeCurrentBinder;
+
+
+
+    //notification
+    public static final String CHANEL_ID = "background_music";
+    public static final int NOTIFICATION_ID = 2;
+    private NotificationManager mManager;
+
+    //music data and mPlayer
+    private PlayList mPlayList;
+    private MediaPlayer mPlayer;
+
+    public enum MusicPlayerState {
+        Stopped, Playing, Paused
+    }
+
+    public static MusicPlayerState sMusicPlayerState;
+
+    //get intent from activity main and notification
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(NEXT)) {
-                Log.d(TAG, "onReceive: NEXT");
+                nextSong();
             } else if (intent.getAction().equals(PAUSE)) {
-                Log.d(TAG, "onReceive: PAUSE");
-            } else if (intent.getAction().equals(PREVIOUS))
-                Log.d(TAG, "onReceive: PREVIOUS");
+                pause();
+            } else if (intent.getAction().equals(PREVIOUS)) {
+                previous();
+            } else if (intent.getAction().equals(SEEK_TO)) {
+                int seekToPosition = intent.getIntExtra(SEEK_TO, 0);
+                if(mPlayer !=null) mPlayer.seekTo(seekToPosition);
+            }
         }
     };
 
     @Override
     public IBinder onBind(Intent intent) {
-        return iBinder;
+        return mTimeCurrentBinder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        //  readMediaFile();
-
-        player = MediaPlayer.create(this, R.raw.onrainyday_beats);
-        player.setLooping(true);
-
+        mTimeCurrentBinder = new TimeCurrentBinder();
+        sMusicPlayerState = MusicPlayerState.Stopped;
         IntentFilter filter = new IntentFilter();
         filter.addAction(MusicService.NEXT);
         filter.addAction(MusicService.PAUSE);
         filter.addAction(MusicService.PREVIOUS);
-        registerReceiver(receiver, filter);
-
+        filter.addAction(SEEK_TO);
+        filter.addAction(PLAY_ONCLICK);
+        registerReceiver(mReceiver, filter);
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        player.start();
-        startForeground(2, createNotification());
 
+        if (intent.getAction().equals(SUPPLY)) {
+            mPlayList = (PlayList) intent.getSerializableExtra(MainActivity.LIST_SONG);
+        } else if (intent.getAction().equals(PLAY)) {
+            int position = intent.getIntExtra(PLAY_ONCLICK,-1);
+            if(position>=0){
+                mPlayList.setCurrentSong(position);
+            }
+            Song song = mPlayList.getListSong().get(mPlayList.getCurrentSong());
+            play(song);
+            startForeground(NOTIFICATION_ID,
+                    createNotification(mPlayList.getListSong().get(mPlayList.getCurrentSong()).getName()));
+
+        }
         return super.onStartCommand(intent, flags, startId);
 
     }
@@ -84,50 +116,26 @@ public class MusicService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(receiver);
-        player.stop();
-        player.release();
+        unregisterReceiver(mReceiver);
+        if(mPlayer !=null) mPlayer.stop();
+        mPlayer.release();
     }
 
-    private void readMediaFile() {
-        ContentResolver resolver = getContentResolver();
-        Uri musicContent = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
-        Cursor cursor = resolver.query(musicContent, null, null, null, null);
-        Log.d(TAG, "readMediaFile: " + cursor.getCount());
 
-
-        if (cursor != null) {
-            cursor.moveToFirst();
-            while (cursor.moveToNext()) {
-
-                String path = cursor.getString(cursor
-                        .getColumnIndex(MediaStore.Audio.Media.DATA));
-                String title = cursor.getString(cursor
-                        .getColumnIndex(MediaStore.Audio.Media.TITLE));
-
-
-                if (path != null && path.endsWith(".mp3")) {
-                    Song song = new Song(title, path);
-                    Log.d(TAG, "readMediaFile: add song");
-                    listSong.add(song);
-                }
-            }
-        }
-
-    }
-
-    private Notification createNotification() {
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    private Notification createNotification(String name) {
+        mManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
             NotificationChannel channel = new NotificationChannel(CHANEL_ID,
                     "Music Control",
                     NotificationManager.IMPORTANCE_HIGH);
-            manager.createNotificationChannel(channel);
+            mManager.createNotificationChannel(channel);
         }
 
-        RemoteViews customNoti = new RemoteViews(getPackageName(), R.layout.custom_notification);
+        RemoteViews customNotification = new RemoteViews(getPackageName(),
+                R.layout.custom_notification);
+        customNotification.setTextViewText(R.id.textSongTitle, name);
 
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
@@ -135,10 +143,11 @@ public class MusicService extends Service {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANEL_ID)
                 .setSmallIcon(R.drawable.ic_queue_music_black_24dp)
                 .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
-                .setCustomContentView(customNoti)
+                .setCustomContentView(customNotification)
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true);
-        setListener(customNoti);
+
+        setListener(customNotification);
 
         return builder.build();
     }
@@ -161,10 +170,85 @@ public class MusicService extends Service {
 
     }
 
-    public class MusicBinder extends Binder {
-        MusicService getInstance() {
-            return MusicService.this;
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+        LocalBroadcastManager bm = LocalBroadcastManager.getInstance(this);
+        Intent sendMessage = new Intent(PREPARED);
+        sendMessage.putExtra(DURATION_SONG, mp.getDuration());
+        bm.sendBroadcast(sendMessage);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        stopForeground(true);
+        stopSelf();
+    }
+
+    private void play(Song song) {
+        if(mPlayer !=null){
+            mPlayer.reset();
+        }
+        if(sMusicPlayerState.equals(MusicPlayerState.Paused)){
+            mPlayer.start();
+        }
+        mPlayer = MediaPlayer.create(getApplicationContext(), Uri.parse(song.getPath()));
+        mPlayer.setOnPreparedListener(this);
+        updateNotification(song.getName());
+        sMusicPlayerState = MusicPlayerState.Playing;
+        mPlayer.start();
+
+    }
+
+    public void nextSong() {
+        if (mPlayList != null) {
+            int position = mPlayList.getCurrentSong();
+            mPlayer.reset();
+            if (position < mPlayList.getListSong().size() - 1) {
+                mPlayList.setCurrentSong(mPlayList.getCurrentSong() + 1);
+                play(mPlayList.getListSong().get(mPlayList.getCurrentSong()));
+            }
         }
     }
 
+    public void pause() {
+        if (mPlayList != null) {
+            if (mPlayer.isPlaying()) {
+                sMusicPlayerState = MusicPlayerState.Paused;
+                mPlayer.pause();
+                sMusicPlayerState = MusicPlayerState.Paused;
+            }
+        }
+    }
+
+    public void previous() {
+        if (mPlayList != null) {
+            mPlayer.reset();
+            int position = mPlayList.getCurrentSong();
+            if (position > 0) {
+                mPlayList.setCurrentSong(mPlayList.getCurrentSong() - 1);
+                play(mPlayList.getListSong().get(mPlayList.getCurrentSong()));
+            }
+        }
+    }
+
+    public void updateNotification(String songTitle) {
+        if (mManager != null) {
+            mManager.notify(NOTIFICATION_ID, createNotification(songTitle));
+        }
+    }
+    public int getCurrentSongPosition(){
+        if(mPlayer !=null)  return mPlayer.getCurrentPosition();
+        return 0;
+    }
+    public int getSongDuration(){
+        if(mPlayer !=null) return mPlayer.getDuration();
+        return 0;
+    }
+    public class TimeCurrentBinder extends Binder{
+        public MusicService getMusicService(){
+            return MusicService.this;
+        }
+    }
 }
--- app/src/main/java/com/example/musicplayer/model/Song.java
+++ app/src/main/java/com/example/musicplayer/model/Song.java
@@ -1,30 +1,32 @@
 package com.example.musicplayer.model;
 
-public class Song {
+import java.io.Serializable;
 
-    private String name;
-    private String path;
+public class Song implements Serializable {
+
+    private String mName;
+    private String mPath;
 
 
     public Song(String name, String path) {
-        this.name = name;
-        this.path = path;
+        this.mName = name;
+        this.mPath = path;
     }
 
     public String getName() {
-        return name;
+        return mName;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.mName = name;
     }
 
     public String getPath() {
-        return path;
+        return mPath;
     }
 
     public void setPath(String path) {
-        this.path = path;
+        this.mPath = path;
     }
 
 }
